f65917c7702585d55848e15ab30d01d28dc33728
Guilherme Segatto <segattooguilherme@gmail.com>
2024-09-13 16:53:10 -0300

    terminado a estilização do post e criando o componente de comentário

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Comment.module.css'
+
+const Comment = () => {
+  return (
+    <div className={styles.comment}>Comment</div>
+  )
+}
+
+export default Comment
