15188f111239385e5c328464081372a9c4f2faa8
Guilherme Segatto <segattooguilherme@gmail.com>
2024-09-12 21:59:55 -0300

    iniciando a criação do componente post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -3,6 +3,7 @@ import './global.css'
 import styles from './App.module.css'
 import Header from './components/Header.jsx'
 import Sidebar from './components/Sidebar.jsx'
+import Post from './components/Post.jsx'
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
      <div className={styles.wrapper}>
        <Sidebar></Sidebar>
         <main>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ipsam doloremque totam nam, error ex voluptatem eligendi reprehenderit quasi voluptatum maiores consectetur quaerat. Odit, ipsam minima labore harum facilis reiciendis.</p>
+          <Post></Post>
+          <Post></Post>
         </main>
      </div>
     </>
---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './Post.module.css'
+
+const Post = () => {
+  return (
+    <article className={styles.post}>
+        <header>
+            <div className={styles.author}>
+                <img src="https://github.com/GuilhermeSegattoo.png" className={styles.avatar} />
+                <div className={styles.authorInfo}>
+                    <strong>Guilherme Segatto</strong>
+                    <span>Web developer</span>
+                </div>
+            </div>
+
+            <time dateTime='2024-09-12 15:08:50'>Publicado há 1h</time>
+
+        </header>
+
+        <div className={styles.content}>
+         <p>Fala galeraa 👋</p>
+
+         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+         <p>👉{'  '}<a href="#"> jane.design/doctorcare</a></p>
+
+         <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+        </div>
+    </article>
+  )
+}
+
+export default Post
